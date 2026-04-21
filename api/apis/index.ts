f33ab1721903be81c9df35bc9f8434b1b9d8d1c3
WebDev-dz <webdevdz2@gmail.com>
2026-04-21 13:38:28 +0100

    chore: remove unused API clients, tests, and enforcement files
    - Delete deprecated API client files (AdsPromotionsApi, ListingsApi, PropertyMatcherApi, SubscriptionsApi)
    - Remove placeholder Next.js config file as configuration is managed elsewhere
    - Clean up obsolete test files and enforcement layer artifacts (tests.js, rls.sql, data-type-validation.txt)
    - Update API index exports to reflect removed clients

--- api/apis/index.ts
+++ api/apis/index.ts
@@ -2,7 +2,6 @@
 /* eslint-disable */
 export * from './AIChatbotApi';
 export * from './AdsApi';
-export * from './AdsPromotionsApi';
 export * from './AppInfoApi';
 export * from './AuthApi';
 export * from './ChatBotApi';
@@ -10,15 +9,12 @@ export * from './DetailsApi';
 export * from './FeaturedListingsApi';
 export * from './FinanceWalletApi';
 export * from './ListingApi';
-export * from './ListingsApi';
 export * from './MemberListingsApi';
 export * from './MemberRequestsApi';
 export * from './MembersApi';
 export * from './NotificationsApi';
 export * from './PasswordResetApi';
 export * from './ProfileApi';
-export * from './PropertyMatcherApi';
 export * from './ReportsApi';
 export * from './ReviewsApi';
-export * from './SubscriptionsApi';
 export * from './WalletApi';
